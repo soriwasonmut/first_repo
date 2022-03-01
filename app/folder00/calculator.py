dffb34da444666c73b256926e19372d9768d3e4e
soriwasonmut <xgao3kids@gmail.com>
2022-02-28 23:36:36 -0600

    add new files

---
+++ app/folder00/calculator.py
@@ -0,0 +1,22 @@
+class Calculator():
+  def add(self, num1,num2):
+    return num1+num2
+  
+  def subtract(self, num1,num2):
+    return num1-num2
+  
+  def multiply(self, num1, num2):
+    return num1*num2
+ 
+  def devide(self, num1, num2):
+    if num2 ==0:
+           return 0
+    return num1/num2  
+
+if __name__ == "__main__":
+
+     calculator = Calculator() 
+     print(calculator.add(1,3))
+     print(calculator.subtract(3,1))
+     print(calculator.multiply(3,1))
+     print(calculator.devide(4,2))
---
+++ test/folder00/test_calculator.py
@@ -0,0 +1,7 @@
+from app.folder00.calculator import Calculator
+
+cal = Calculator()
+
+def test_add():
+    assert cal.add(1,1) == 2
+    assert cal.add(1,2) == 3
